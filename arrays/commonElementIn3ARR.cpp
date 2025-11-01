db899d4ef8719f6226b7db36e0b6f60ea0ea6db9
Advayaverma <advaya2208@gmail.com>
2025-11-01 22:19:24 +0530

    Common in 3 Sorted Arrays  GfG

---
+++ arrays/commonElementIn3ARR.cpp
@@ -0,0 +1,36 @@
+//GFG Common in 3 Sorted Arrays
+
+class Solution {
+  public:
+    // Function to find common elements in three arrays.
+    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2, vector<int> &arr3) {
+        // Code Here
+        vector<int> result;
+        int i=0,j=0,k=0;
+        
+        while (i < arr1.size() && j < arr2.size() && k < arr3.size()){
+            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]){
+                if (result.empty() || result.back() != arr1[i]){
+                    result.push_back(arr1[i]);
+                }
+    
+                ++i;
+                ++j;
+                ++k;
+            }
+            
+            else if (arr1[i] < arr2[j]){
+                ++i;
+            }
+            
+            else if (arr2[j] < arr3[k]){
+                ++j;
+            }
+            
+            else {
+                ++k;
+            }
+        }
+        return result;
+    }
+};
