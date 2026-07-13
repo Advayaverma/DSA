376ebb78dbc88afe345ef69907634f321b03d6a5
Advayaverma <advaya2208@gmail.com>
2026-07-13 22:01:55 +0530

    leetcode 704

---
+++ Searching and Sorting/binarySearch.cpp
@@ -0,0 +1,26 @@
+// leetcode 704  Binary Search
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int high = nums.size() - 1;
+        int low = 0;
+        
+
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target){
+                return mid;
+            }
+            else if (nums[mid] > target){
+                high = mid - 1;
+            }
+            else if (nums[mid] < target){
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
