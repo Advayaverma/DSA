69caefc84a0526eb78f68061a58e5ef78971206b
Advayaverma <advaya2208@gmail.com>
2026-07-15 23:48:08 +0530

    leetcode 136

---
+++ arrays/singleNumber.cpp
@@ -0,0 +1,21 @@
+// Leetcode 136: Single Number
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map <int, int> freq;
+
+        for (int i = 0; i < nums.size(); i++){
+            int num = nums[i];
+            freq[num] += 1;
+        }
+
+        unordered_map <int, int> :: iterator it;
+        for (it = freq.begin(); it != freq.end(); it++){
+            if (it->second == 1){
+                return it->first;
+            }
+        }
+        return -1;
+    }
+};
