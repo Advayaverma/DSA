5d36e55bc70d10a9786f6bd0edaf6f0ade7b0095
Advayaverma <advaya2208@gmail.com>
2026-07-19 22:16:39 +0530

    leetcode 852

--- Searching and Sorting/peakIndex.cpp
+++ Searching and Sorting/peakIndex.cpp
@@ -19,3 +19,27 @@ public:
         return low;
     }
 };
+
+
+// STORE AND COMPUTE SOLUTION
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0, high = arr.size() - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                low = mid + 1;
+            }
+            else {
+                ans = mid;
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
