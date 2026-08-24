8d12abb600d29911fd3bf00cfb4c2d6bbbb8feee
Advayaverma <160547019+Advayaverma@users.noreply.github.com>
2026-08-24 21:25:19 +0530

    Time: 25 ms (28.18%), Space: 107.1 MB (88.39%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int pickP = 0;
+        int travelP = 0;
+        int lastHouseP = 0;
+
+        int pickM = 0;
+        int travelM = 0;
+        int lastHouseM = 0;
+
+        int pickG = 0;
+        int travelG = 0;
+        int lastHouseG = 0;
+
+        for (int i = 0; i < garbage.size(); i++){
+            string currHouse = garbage[i];
+            for (int j = 0; j < currHouse.size(); j++){
+                char garbageType = currHouse[j];
+                if (garbageType == 'P'){
+                    pickP++;
+                    lastHouseP = i;
+                }
+
+                if (garbageType == 'G'){
+                    pickG++;
+                    lastHouseG = i;
+                }
+
+                if (garbageType == 'M'){
+                    pickM++;
+                    lastHouseM = i;
+                }
+            }
+        }
+
+        for (int i = 0; i < lastHouseP; i++){
+            travelP = travelP + travel[i];
+        }
+
+        for (int i = 0; i < lastHouseG; i++){
+            travelG = travelG + travel[i];
+        }
+
+        for (int i = 0; i < lastHouseM; i++){
+            travelM = travelM + travel[i];
+        }
+        int totalPickTime = pickP + pickG + pickM;
+        int totalTravelTime = travelP + travelG + travelM;
+
+        int totalTime = totalPickTime + totalTravelTime;
+        return totalTime;
+    }
+};
